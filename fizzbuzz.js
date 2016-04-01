e3176cd98c2055717291f8c493e8c42fd36b2c2a
Eric Humpert <EricMac@Ricks-MBP.attlocal.net>
2016-03-31 21:52:31 -0500

    added a forgotten semicolon to the variable statement

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,6 +1,6 @@
 //Write a program that prints the numbers from 1 to 30. But for multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz". For numbers which are multiples of both three and five print "FizzBuzz".
 
-var count = 30
+var count = 30;
 
 for (var i = 0; i < count; i++) {
   if (i % 3 == 0 && i % 5 == 0) {
