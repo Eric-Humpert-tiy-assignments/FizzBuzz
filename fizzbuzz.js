a6987a7223a37c277e195981202070443eb3f2be
Eric Humpert <EricMac@Ricks-MacBook-Pro.local>
2016-04-01 13:00:15 -0500

    fixed bugs in the for and while loops in the assignment

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -2,17 +2,29 @@
 
 var count = 30;
 
-for (var i = 0; i < count; i++) {
+for (var i = 1; i <= count; i++) {
   if (i % 3 == 0 && i % 5 == 0) {
-        console.log('FizzBuzz');
-  }
-  if (i % 3 == 0) {
-    console.log('Fizz');
-  }
-  else if (i % 5 == 0) {
-    console.log('Buzz');
-  }
-  else {
+        console.log(i, 'FizzBuzz');
+  }  else if (i % 3 == 0) {
+    console.log(i, 'Fizz');
+  }  else if (i % 5 == 0) {
+    console.log(i, 'Buzz');
+  }  else {
     console.log(i);
   }
+}
+
+var counter = 1;
+
+while (counter <= 30) {
+  if (counter % 3 == 0 && counter % 5 == 0) {
+    console.log('FizzBuzz');
+  } else if (counter % 3 == 0) {
+    console.log('Fizz');
+  } else if (counter % 5 == 0) {
+    console.log('Buzz');
+  } else {
+    console.log(counter);
+  }
+  counter += 1;
 }
