f0f054f9c6f2a6d311b9c734a3c2e68eacf8c310
Eric Humpert <EricMac@Ricks-MBP.attlocal.net>
2016-03-31 21:41:44 -0500

    configured the if else statement to replace the divisble numbers replaced with fizz, buzz, or fizzbuzz

---
+++ fizzbuzz.js
@@ -0,0 +1,18 @@
+//Write a program that prints the numbers from 1 to 30. But for multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz". For numbers which are multiples of both three and five print "FizzBuzz".
+
+var count = 30
+
+for (var i = 0; i < count; i++) {
+  if (i % 3 == 0 && i % 5 == 0) {
+        console.log('FizzBuzz');
+  }
+  if (i % 3 == 0) {
+    console.log('Fizz');
+  }
+  else if (i % 5 == 0) {
+    console.log('Buzz');
+  }
+  else {
+    console.log(i);
+  }
+}
